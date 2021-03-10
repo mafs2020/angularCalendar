8618ccf15031f10d8779bad7853dbebae0c8fcff
MAFS <martin117h@gmail.com>
2021-03-09 22:44:08 -0600

    funciona solo falta el color

--- src/app/pages/calendario/calendario.component.ts
+++ src/app/pages/calendario/calendario.component.ts
@@ -1,7 +1,8 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable @typescript-eslint/quotes */
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { FormControl, FormArray, FormGroup } from '@angular/forms';
+import { FormControl, FormArray, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import {
   startOfDay,
   endOfDay,
@@ -20,6 +21,7 @@ import {
   CalendarView,
 } from 'angular-calendar';
 import { FlatpickrOptions } from 'ng2-flatpickr';
+import { CalendarioService } from 'src/app/services/calendario.service';
 
 const colors: any = {
   red: {
@@ -47,58 +49,34 @@ export class CalendarioComponent implements OnInit {
   CalendarView = CalendarView;
   viewDate: Date = new Date();
   refresh: Subject<any> = new Subject();
-
-  events: CalendarEvent[] = [
-    {
-      start: subDays(startOfDay(new Date()), 1),
-      end: addDays(new Date(), 1),
-      title: 'A 3 day event',
-      color: colors.red,
-      allDay: true,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true,
-      },
-      draggable: true,
-    },
-    {
-      start: startOfDay(new Date()),
-      title: 'An event with no end date',
-      color: colors.yellow,
-      // actions: this.actions,
-    },
-    {
-      start: subDays(endOfMonth(new Date()), 3),
-      end: addDays(endOfMonth(new Date()), 3),
-      title: 'A long event that spans 2 months',
-      color: colors.blue,
-      allDay: true,
-    },
-    {
-      start: addHours(startOfDay(new Date()), 2),
-      end: addHours(new Date(), 2),
-      title: 'A draggable and resizable event',
-      color: colors.yellow,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true,
-      },
-      draggable: true,
-    },
-  ];
-  start: FormControl = new FormControl();
-  configuracion: FlatpickrOptions  = {
-    // (ngModelChange)="refresh.next()"
-    altInput: true,
-    convertModelValue: true,
-    enableTime: true,
-    dateFormat: "Y-m-dTH:i",
-    altFormat: "F j Y H:i",
-    placeholder: "Not set",
-  };
-  constructor() { }
+  formulario: FormGroup;
+  events: CalendarEvent[] = [];
+  configsStart: FlatpickrOptions[] = [];
+  configsEnd: FlatpickrOptions[] = [];
+  i: number;
+  constructor(private fb: FormBuilder, private calendarioService: CalendarioService) { }
 
   ngOnInit(): void {
+    this.iniciarFormulario();
+    this.calendarioService.getEventos().subscribe(data => {
+      data.map(e => this.agregarFecha(e));
+      data.map(e => this.addFecha(e));
+      this.events = [...this.events, ...data.map(e => {
+        return {
+          title: e.title,
+          color: colors.red,
+          // colors: colors.red,
+          start: startOfDay(new Date(e.start)),
+          end: endOfDay(new Date(e.end)),
+          altInput: true,
+          convertModelValue: true,
+          enableTime: true,
+          dateFormat: "Y-m-dTH:i",
+          altFormat: "F j Y H:i",
+          placeholder: "Not set",
+        };
+      })];
+    });
   }
 
   eventTimesChanged({
@@ -126,7 +104,6 @@ export class CalendarioComponent implements OnInit {
         start: startOfDay(new Date()),
         end: endOfDay(new Date()),
         color: colors.red,
-        draggable: true,
         resizable: {
           beforeStart: true,
           afterEnd: true,
@@ -135,8 +112,126 @@ export class CalendarioComponent implements OnInit {
     ];
   }
 
-  deleteEvent(eventToDelete: CalendarEvent) {
-    this.events = this.events.filter((event) => event !== eventToDelete);
+  deleteEvent(i: number) {
+    this.events.splice(i,1);
+    this.refresh.next();
+  }
+
+  iniciarFormulario() {
+    this.formulario = this.fb.group({
+      fecha: this.fb.array([])
+    });
+  }
+
+  get fechas(): FormArray {
+    return this.formulario.get('fecha') as FormArray;
+  }
+
+  newFecha(e: CalendarEvent): FormGroup {
+    return this.fb.group({
+      fecha: [new Date(e.start)],
+      title: [e.title],
+      primary: ['#ad2121'],
+      color: ['#ad2121']
+      // color: { updateOn: '' }
+    });
+  }
+
+  addFecha(e: CalendarEvent): void {
+    this.fechas.push(this.newFecha(e));
+  }
+
+  agregarFecha(e: CalendarEvent) {
+    this.configsStart.push({
+      altInput: true,
+      title: e.title,
+      color: colors.red,
+      onChange: (date, datestring) => this.cambiarStart(date, datestring),
+      defaultDate: startOfDay(new Date(e.start)),
+      convertModelValue: true,
+      enableTime: true,
+      dateFormat: "Y-m-dTH:i",
+      altFormat: "F j Y H:i",
+      placeholder: "Not set",
+    });
+
+    this.configsEnd.push({
+      altInput: true,
+      title: e.title,
+      color: colors.red,
+      onChange: (date, datestring) => this.cambiarEnd(date, datestring),
+      defaultDate: endOfDay(new Date(e.end)),
+      convertModelValue: true,
+      enableTime: true,
+      dateFormat: "Y-m-dTH:i",
+      altFormat: "F j Y H:i",
+      placeholder: "Not set",
+    });
+    this.refresh.next();
+  }
+
+  index(i: number) {
+    this.i = i;
+    console.log(this.i);
+  }
+
+  cambiarStart(date, dateString){
+    this.events[this.i].start = date[0];
+    this.refresh.next();
+  }
+
+  cambiarEnd(date, dateString){
+    this.events[this.i].end = date[0];
+    this.refresh.next();
+  }
+
+  removeSkill(i: number) {
+    this.fechas.removeAt(i);
+    this.deleteEvent(i);
+  }
+
+  x(i: number) {
+    console.log(i);
+    console.log(this.fechas.controls[i].get('title').value);
   }
 
 }
+
+
+
+
+
+// {
+//   start: subDays(startOfDay(new Date()), 1),
+//   end: addDays(new Date(), 1),
+//   title: 'A 3 day event',
+//   color: colors.red,
+//   allDay: true,
+//   resizable: {
+//     beforeStart: true,
+//     afterEnd: true,
+//   },
+// },
+// {
+//   start: startOfDay(new Date()),
+//   title: 'An event with no end date',
+//   color: colors.yellow,
+//   // actions: this.actions,
+// },
+// {
+//   start: subDays(endOfMonth(new Date()), 3),
+//   end: addDays(endOfMonth(new Date()), 3),
+//   title: 'A long event that spans 2 months',
+//   color: colors.blue,
+//   allDay: true,
+// },
+// {
+//   start: addHours(startOfDay(new Date()), 2),
+//   end: addHours(new Date(), 2),
+//   title: 'A draggable and resizable event',
+//   color: colors.yellow,
+//   resizable: {
+//     beforeStart: true,
+//     afterEnd: true,
+//   },
+// },
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,10 +5,11 @@ import { rutasPagesModule } from './pages.routes';
 
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { Ng2FlatpickrModule } from 'ng2-flatpickr';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Ng2FlatpickrModule } from 'ng2-flatpickr';
+
 import { CalendarioComponent } from './calendario/calendario.component';
 
 @NgModule({
@@ -21,7 +22,6 @@ import { CalendarioComponent } from './calendario/calendario.component';
     rutasPagesModule,
     FormsModule,
     ReactiveFormsModule,
-    // FlatpickrModule.forRoot(),
     Ng2FlatpickrModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
